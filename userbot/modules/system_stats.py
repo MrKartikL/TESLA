6987204631125fb9d8c4d1dbbb1703aca835a167
Shéikh Adnan <manofuranium@gmail.com>
2020-06-09 19:51:35 +0530

    Tesla

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -131,13 +131,14 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`My Detail Ubot `\n"
+    await alive.edit("`Heya , This is TESLA `\n"
+		     f"`And Running on `\n"
                      f"> `Telethon : v{version.__version__} `\n"
                      f"> `Python : v{python_version()} `\n"
 	                 "===================== \n"
                      f"`User : `{DEFAULTUSER} \n"
 		             "===================== \n"
-                     f"__Running on {UPSTREAM_REPO_BRANCH}__ \n")
+                     f"__Running on branch :- {UPSTREAM_REPO_BRANCH}__ \n")
 
 
 @register(outgoing=True, pattern="^.aliveu")
